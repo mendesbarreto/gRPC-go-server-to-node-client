9490649ade9b0f8a5fd5faf41cc55108e452d192
Douglas <mendes-barreto@Live.com>
2023-07-17 10:25:34 -0400

    chore: Add test with gRPC

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"log"
+	"net"
+
+	"github.com/mendesbarreto/server-grpc-go/proto/gen"
+	"google.golang.org/grpc"
+)
+
+type server struct{}
+
+func (s *server) GetUser(ctx context.Context, req *gen.GetUserRequest) (*gen.GetUserResponse, error) {
+	return &gen.GetUserResponse{
+		Name:  "Douglas Mendes",
+		Email: "mendes-barreto@live.com",
+		Phone: "11999999999",
+	}, nil
+}
 
 func main() {
+	lis, err := net.Listen("tcp", ":50051")
+
+	if err != nil {
+		log.Fatalln("failed to listen %v", err)
+	}
+
+	grpcServer := grpc.NewServer()
+
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatalln("failed to server: %v", err)
+	}
+
 	fmt.Print("Hello HotReload In Go!\n")
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,14 @@
-module github.com/mendesbarreto/hot-reload-golang-template
+module github.com/mendesbarreto/server-grpc-go
 
 go 1.19
+
+require (
+	github.com/golang/protobuf v1.5.3 // indirect
+	golang.org/x/net v0.12.0 // indirect
+	golang.org/x/sys v0.10.0 // indirect
+	golang.org/x/text v0.11.0 // indirect
+	google.golang.org/genproto v0.0.0-20230706204954-ccb25ca9f130 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20230711160842-782d3b101e98 // indirect
+	google.golang.org/grpc v1.56.2 // indirect
+	google.golang.org/protobuf v1.31.0 // indirect
+)
