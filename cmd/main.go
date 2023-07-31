c7876df46fe4c85616e256d3616ba262c3609b0a
Douglas <mendes-barreto@Live.com>
2023-07-31 11:40:43 -0400

    chore: Add chuck norris fact to go lang api

--- cmd/main.go
+++ cmd/main.go
@@ -2,23 +2,61 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net"
+	"net/http"
 
-	"github.com/mendesbarreto/server-grpc-go/proto"
+	gen "github.com/mendesbarreto/server-grpc-go/proto"
 	"google.golang.org/grpc"
 )
 
+var chuckNorrisFactsURL string = "https://api.chucknorris.io/jokes/random"
+
 type server struct {
-	gen.UserServiceServer
+	gen.ChuckNorrisFactServiceServer
 }
 
-func (s *server) GetUser(ctx context.Context, req *gen.GetUserRequest) (*gen.GetUserResponse, error) {
-	return &gen.GetUserResponse{
-		Name:  "Douglas Mendes",
-		Email: "mendes-barreto@live.com",
-		Phone: "11999999999",
+type ChuckNorrisFact struct {
+	IconUrl string `json:"icon_url"`
+	Id      string `json:"id"`
+	Url     string `json:"url"`
+	Value   string `json:"value"`
+}
+
+func (s *server) GetRandomFact(ctx context.Context, req *gen.GetRandomJokeRequest) (*gen.GetRandomJokeResponse, error) {
+
+	response, err := http.Get(chuckNorrisFactsURL)
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer response.Body.Close()
+
+	if response.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("status code was %d", response.StatusCode)
+	}
+
+	body, err := ioutil.ReadAll(response.Body)
+
+	if err != nil {
+		return nil, fmt.Errorf("Problem to read the response body %v", err)
+	}
+
+	var fact ChuckNorrisFact
+
+	if err := json.Unmarshal(body, &fact); err != nil {
+		return nil, fmt.Errorf("Could not unmarshal the response %v", err)
+	}
+
+	return &gen.GetRandomJokeResponse{
+		Id:      fact.Id,
+		IconUrl: fact.IconUrl,
+		Url:     &fact.Url,
+		Value:   fact.Value,
 	}, nil
 }
 
@@ -32,7 +70,8 @@ func main() {
 	grpcServer := grpc.NewServer()
 
 	s := server{}
-	gen.RegisterUserServiceServer(grpcServer, &s)
+	gen.RegisterChuckNorrisFactServiceServer(grpcServer, &s)
+
 	if err := grpcServer.Serve(lis); err != nil {
 		log.Fatalln("failed to server: %v", err)
 	}
